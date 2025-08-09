1b8b1e6f729390d143163a3a0d10dd9150eb10da
Hans Bakker <jjabakker@icloud.com>
2025-08-09 09:58:44 +0200

    All new

--- src/main/java/Paint/PaintSquare.java
+++ src/main/java/Paint/PaintSquare.java
@@ -1,24 +1,22 @@
 package Paint;
 
-import tech.tablesaw.api.*;
-import tech.tablesaw.io.csv.CsvReadOptions;
-import tech.tablesaw.api.Table;
-import tech.tablesaw.api.ColumnType;
-import tech.tablesaw.io.csv.CsvReadOptions;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class PaintSquare {
-    private List<PaintTrack> paintTracks = new ArrayList<>();
-    private double startX;
-    private double startY;
-    private double endX;
-    private double endY;
+    private String uniqueKey;
+    private int recordingSequenceNr;
+    private int conditionNumber;
+    private int replicateNumber;
+    private int squareNumber;
+    private int rowNumber;
+    private int colNumber;
+    private int labelNumber;
+    private int cellId;
+    private int numberSpots;
+    private int numberTracks;
+    private double X0;
+    private double Y0;
+    private double X1;
+    private double Y1;
+    private boolean selected;
     private double variability;
     private double density;
     private double densityRatio;
@@ -43,20 +41,62 @@ public class PaintSquare {
     private boolean squareManuallyExcluded;
     private boolean imageExcluded;
 
-
-    public PaintSquare(double startX, double startY, double endX, double endY,
-                       double variability, double density, double densityRatio, double tau, double rSquared,
-                       double medianDiffusionCoefficient, double meanDiffusionCoefficient,
-                       double medianDiffusionCoefficientExt, double meanDiffusionCoefficientExt,
-                       double medianLongTrackDuration, double medianShortTrackDuration,
-                       double medianDisplacement, double maxDisplacement, double totalDisplacement,
-                       double medianMaxSpeed, double maxMaxSpeed, double medianMeanSpeed, double maxMeanSpeed,
-                       double maxTrackDuration, double totalTrackDuration, double medianTrackDuration,
-                       boolean squareManuallyExcluded, boolean imageExcluded) {
-        this.startX = startX;
-        this.startY = startY;
-        this.endX = endX;
-        this.endY = endY;
+    // Constructor
+    public PaintSquare(String uniqueKey,
+                       int recordingSequenceNr,
+                       int conditionNumber,
+                       int replicateNumber,
+                       int squareNumber,
+                       int rowNumber,
+                       int colNumber,
+                       int labelNumber,
+                       int cellId,
+                       int numberSpots,
+                       int numberTracks,
+                       double X0,
+                       double Y0,
+                       double X1,
+                       double Y1,
+                       boolean selected,
+                       double variability,
+                       double density,
+                       double densityRatio,
+                       double tau,
+                       double rSquared,
+                       double medianDiffusionCoefficient,
+                       double meanDiffusionCoefficient,
+                       double medianDiffusionCoefficientExt,
+                       double meanDiffusionCoefficientExt,
+                       double medianLongTrackDuration,
+                       double medianShortTrackDuration,
+                       double medianDisplacement,
+                       double maxDisplacement,
+                       double totalDisplacement,
+                       double medianMaxSpeed,
+                       double maxMaxSpeed,
+                       double medianMeanSpeed,
+                       double maxMeanSpeed,
+                       double maxTrackDuration,
+                       double totalTrackDuration,
+                       double medianTrackDuration,
+                       boolean squareManuallyExcluded,
+                       boolean imageExcluded) {
+        this.uniqueKey = uniqueKey;
+        this.recordingSequenceNr = recordingSequenceNr;
+        this.conditionNumber = conditionNumber;
+        this.replicateNumber = replicateNumber;
+        this.squareNumber = squareNumber;
+        this.rowNumber = rowNumber;
+        this.colNumber = colNumber;
+        this.labelNumber = labelNumber;
+        this.cellId = cellId;
+        this.numberSpots = numberSpots;
+        this.numberTracks = numberTracks;
+        this.X0 = X0;
+        this.Y0 = Y0;
+        this.X1 = X1;
+        this.Y1 = Y1;
+        this.selected = selected;
         this.variability = variability;
         this.density = density;
         this.densityRatio = densityRatio;
@@ -82,299 +122,5 @@ public class PaintSquare {
         this.imageExcluded = imageExcluded;
     }
 
-    // Getters and setters for all fields
-
-    public List<PaintTrack> getTracks() {
-        return paintTracks;
-    }
-
-    public void setTracks(List<PaintTrack> paintTracks) {
-        this.paintTracks = paintTracks;
-    }
-
-    public double getStartX() {
-        return startX;
-    }
-
-    public void setStartX(double startX) {
-        this.startX = startX;
-    }
-
-    public double getStartY() {
-        return startY;
-    }
-
-    public void setStartY(double startY) {
-        this.startY = startY;
-    }
-
-    public double getEndX() {
-        return endX;
-    }
-
-    public void setEndX(double endX) {
-        this.endX = endX;
-    }
-
-    public double getEndY() {
-        return endY;
-    }
-
-    public void setEndY(double endY) {
-        this.endY = endY;
-    }
-
-    public double getVariability() {
-        return variability;
-    }
-
-    public void setVariability(double variability) {
-        this.variability = variability;
-    }
-
-    public double getDensity() {
-        return density;
-    }
-
-    public void setDensity(double density) {
-        this.density = density;
-    }
-
-    public double getDensityRatio() {
-        return densityRatio;
-    }
-
-    public void setDensityRatio(double densityRatio) {
-        this.densityRatio = densityRatio;
-    }
-
-    public double getTau() {
-        return tau;
-    }
-
-    public void setTau(double tau) {
-        this.tau = tau;
-    }
-
-    public double getRSquared() {
-        return rSquared;
-    }
-
-    public void setRSquared(double rSquared) {
-        this.rSquared = rSquared;
-    }
-
-    public double getMedianDiffusionCoefficient() {
-        return medianDiffusionCoefficient;
-    }
-
-    public void setMedianDiffusionCoefficient(double medianDiffusionCoefficient) {
-        this.medianDiffusionCoefficient = medianDiffusionCoefficient;
-    }
-
-    public double getMeanDiffusionCoefficient() {
-        return meanDiffusionCoefficient;
-    }
-
-    public void setMeanDiffusionCoefficient(double meanDiffusionCoefficient) {
-        this.meanDiffusionCoefficient = meanDiffusionCoefficient;
-    }
-
-    public double getMedianDiffusionCoefficientExt() {
-        return medianDiffusionCoefficientExt;
-    }
-
-    public void setMedianDiffusionCoefficientExt(double medianDiffusionCoefficientExt) {
-        this.medianDiffusionCoefficientExt = medianDiffusionCoefficientExt;
-    }
-
-    public double getMeanDiffusionCoefficientExt() {
-        return meanDiffusionCoefficientExt;
-    }
-
-    public void setMeanDiffusionCoefficientExt(double meanDiffusionCoefficientExt) {
-        this.meanDiffusionCoefficientExt = meanDiffusionCoefficientExt;
-    }
-
-    public double getMedianLongTrackDuration() {
-        return medianLongTrackDuration;
-    }
-
-    public void setMedianLongTrackDuration(double medianLongTrackDuration) {
-        this.medianLongTrackDuration = medianLongTrackDuration;
-    }
-
-    public double getMedianShortTrackDuration() {
-        return medianShortTrackDuration;
-    }
-
-    public void setMedianShortTrackDuration(double medianShortTrackDuration) {
-        this.medianShortTrackDuration = medianShortTrackDuration;
-    }
-
-    public double getMedianDisplacement() {
-        return medianDisplacement;
-    }
-
-    public void setMedianDisplacement(double medianDisplacement) {
-        this.medianDisplacement = medianDisplacement;
-    }
-
-    public double getMaxDisplacement() {
-        return maxDisplacement;
-    }
-
-    public void setMaxDisplacement(double maxDisplacement) {
-        this.maxDisplacement = maxDisplacement;
-    }
-
-    public double getTotalDisplacement() {
-        return totalDisplacement;
-    }
-
-    public void setTotalDisplacement(double totalDisplacement) {
-        this.totalDisplacement = totalDisplacement;
-    }
-
-    public double getMedianMaxSpeed() {
-        return medianMaxSpeed;
-    }
-
-    public void setMedianMaxSpeed(double medianMaxSpeed) {
-        this.medianMaxSpeed = medianMaxSpeed;
-    }
-
-    public double getMaxMaxSpeed() {
-        return maxMaxSpeed;
-    }
-
-    public void setMaxMaxSpeed(double maxMaxSpeed) {
-        this.maxMaxSpeed = maxMaxSpeed;
-    }
-
-    public double getMedianMeanSpeed() {
-        return medianMeanSpeed;
-    }
-
-    public void setMedianMeanSpeed(double medianMeanSpeed) {
-        this.medianMeanSpeed = medianMeanSpeed;
-    }
-
-    public double getMaxMeanSpeed() {
-        return maxMeanSpeed;
-    }
-
-    public void setMaxMeanSpeed(double maxMeanSpeed) {
-        this.maxMeanSpeed = maxMeanSpeed;
-    }
-
-    public double getMaxTrackDuration() {
-        return maxTrackDuration;
-    }
-
-    public void setMaxTrackDuration(double maxTrackDuration) {
-        this.maxTrackDuration = maxTrackDuration;
-    }
-
-    public double getTotalTrackDuration() {
-        return totalTrackDuration;
-    }
-
-    public void setTotalTrackDuration(double totalTrackDuration) {
-        this.totalTrackDuration = totalTrackDuration;
-    }
-
-    public double getMedianTrackDuration() {
-        return medianTrackDuration;
-    }
-
-    public void setMedianTrackDuration(double medianTrackDuration) {
-        this.medianTrackDuration = medianTrackDuration;
-    }
-
-    public boolean isSquareManuallyExcluded() {
-        return squareManuallyExcluded;
-    }
-
-    public void setSquareManuallyExcluded(boolean squareManuallyExcluded) {
-        this.squareManuallyExcluded = squareManuallyExcluded;
-    }
-
-    public boolean isImageExcluded() {
-        return imageExcluded;
-    }
-
-    public void setImageExcluded(boolean imageExcluded) {
-        this.imageExcluded = imageExcluded;
-    }
-
-    // CSV serialization
-    public String toCSV() {
-        return String.join(",",
-                String.valueOf(startX),
-                String.valueOf(startY),
-                String.valueOf(endX),
-                String.valueOf(endY),
-                String.valueOf(variability),
-                String.valueOf(density),
-                String.valueOf(densityRatio),
-                String.valueOf(tau),
-                String.valueOf(rSquared),
-                String.valueOf(medianDiffusionCoefficient),
-                String.valueOf(meanDiffusionCoefficient),
-                String.valueOf(medianDiffusionCoefficientExt),
-                String.valueOf(meanDiffusionCoefficientExt),
-                String.valueOf(medianLongTrackDuration),
-                String.valueOf(medianShortTrackDuration),
-                String.valueOf(medianDisplacement),
-                String.valueOf(maxDisplacement),
-                String.valueOf(totalDisplacement),
-                String.valueOf(medianMaxSpeed),
-                String.valueOf(maxMaxSpeed),
-                String.valueOf(medianMeanSpeed),
-                String.valueOf(maxMeanSpeed),
-                String.valueOf(maxTrackDuration),
-                String.valueOf(totalTrackDuration),
-                String.valueOf(medianTrackDuration),
-                String.valueOf(squareManuallyExcluded),
-                String.valueOf(imageExcluded)
-        );
-    }
-
-    // CSV deserialization
-//    public static Square fromCSV(String csvLine) {
-//        String[] parts = csvLine.split(",", -1);
-//        if (parts.length != 27) {
-//            throw new IllegalArgumentException("CSV line does not have 27 fields: " + csvLine);
-//        }
-//
-//        return new Square(
-//                Double.parseDouble(parts[0]),
-//                Double.parseDouble(parts[1]),
-//                Double.parseDouble(parts[2]),
-//                Double.parseDouble(parts[3]),
-//                Double.parseDouble(parts[4]),
-//                Double.parseDouble(parts[5]),
-//                Double.parseDouble(parts[6]),
-//                Double.parseDouble(parts[7]),
-//                Double.parseDouble(parts[8]),
-//                Double.parseDouble(parts[9]),
-//                Double.parseDouble(parts[10]),
-//                Double.parseDouble(parts[11]),
-//                Double.parseDouble(parts[12]),
-//                Double.parseDouble(parts[13]),
-//                Double.parseDouble(parts[14]),
-//                Double.parseDouble(parts[15]),
-//                Double.parseDouble(parts[16]),
-//                Double.parseDouble(parts[17]),
-//                Double.parseDouble(parts[18]),
-//                Double.parseDouble(parts[19]),
-//                Double.parseDouble(parts[20]),
-//                Double.parseDouble(parts[21]),
-//                Double.parseDouble(parts[22]),
-//                Double.parseDouble(parts[23]),
-//                Boolean.parseBoolean(parts[24]),
-//                Boolean.parseBoolean(parts[25])
-//        );
-//    }
+    // getters and setters here ...
 }
---
+++ src/main/java/Paint/PaintSquareLoader.java
@@ -0,0 +1,114 @@
+package Paint;
+
+import tech.tablesaw.api.Row;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaintSquareLoader {
+
+    public static void main(String[] args) {
+
+        List<PaintSquare> squares = loadSquares(Paths.get("/Users/hans/Downloads/221012/All Squares.csv"), null);
+        System.out.println("All tracks count: " + squares.size());
+
+        squares = loadAllSquares(Paths.get("/Users/hans/Downloads/221012/All Squares.csv"));
+        System.out.println("All squares count: " + squares.size());
+
+        List<PaintSquare> filteredSquares = loadSquares(Paths.get("/Users/hans/Downloads/221012/All Squares.csv"), "221012-Exp-1-A1-2-threshold-8");
+        System.out.println("Filtered squares count: " + filteredSquares.size());
+    }
+
+
+    // Load all the squares from CSV
+
+    public static List<PaintSquare> loadAllSquares(Path csvPath) {
+        return loadSquares(csvPath, null);
+    }
+
+
+    // Load squares from CSV, optionally filtered by recordingName.
+    // If the recordingName is null or empty, all tracks are loaded.
+
+    public static List<PaintSquare> loadSquares(Path csvPath, String recordingName) {
+
+        Table table = null;
+
+        try {
+            table = Table.read().csv(csvPath.toFile());
+        } catch (Exception e) {
+            String errorMsg = e.toString(); // e.g. "java.io.FileNotFoundException: /path/to/file (No such file or directory)"
+            int colonIndex = errorMsg.lastIndexOf(":");
+            String messageAfterColon = (colonIndex != -1) ? errorMsg.substring(colonIndex + 1).trim() : errorMsg;
+            System.err.println("Failed to read squares file: " + messageAfterColon);
+            System.exit(-1);
+        }
+
+        if (recordingName != null && !recordingName.isEmpty() && table.containsColumn("Ext Recording Name")) {
+            if (table.column("Ext Recording Name") instanceof StringColumn) {
+                StringColumn recordingCol = table.stringColumn("Ext Recording Name");
+                table = table.where(recordingCol.isEqualTo(recordingName));
+            }
+        }
+        return createSquares(table);
+    }
+
+    private static List<PaintSquare> createSquares(Table table) {
+        List<PaintSquare> squares = new ArrayList<>();
+
+        try {
+            for (Row row : table) {
+                PaintSquare square = new PaintSquare(
+                        row.getString("Unique Key"),
+                        row.getInt("Recording Sequence Nr"),
+                        row.getInt("Condition Nr"),
+                        row.getInt("Replicate Nr"),
+                        row.getInt("Square Nr"),
+                        row.getInt("Row Nr"),
+                        row.getInt("Col Nr"),
+                        row.getInt("label Nr"),
+                        row.getInt("Cell Id"),
+                        row.getInt("Nr Spots"),
+                        row.getInt("Nr Tracks"),
+                        row.getDouble("X0"),
+                        row.getDouble("Y0"),
+                        row.getDouble("X1"),
+                        row.getDouble("Y1"),
+                        row.getBoolean("Selected"),
+                        row.getDouble("Variability"),
+                        row.getDouble("Density"),
+                        row.getDouble("Density Ratio"),
+                        Double.valueOf (row.getInt("Tau")),
+                        row.getDouble("R Squared"),
+                        row.getDouble("Median Diffusion Coefficient"),
+                        row.getDouble("Mean Diffusion Coefficient"),
+                        row.getDouble("Median Diffusion Coefficient Ext"),
+                        row.getDouble("Mean Diffusion Coefficient Ext"),
+                        row.getDouble("Median Long Track Duration"),
+                        row.getDouble("Median Short Track Duration"),
+                        row.getDouble("Median Displacement"),
+                        row.getDouble("Max Displacement"),
+                        row.getDouble("Total Displacement"),
+                        row.getDouble("Median Max Speed"),
+                        row.getDouble("Max Max Speed"),
+                        row.getDouble("Median Mean Speed"),
+                        row.getDouble("Max Mean Speed"),
+                        row.getDouble("Max Track Duration"),
+                        row.getDouble("Total Track Duration"),
+                        row.getDouble("Median Track Duration"),
+                        row.getBoolean("Square Manually Excluded"),
+                        row.getBoolean("Image Excluded")
+                );
+                squares.add(square);
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to load squares - columns contain data in wrong format: " + e.getMessage());
+            System.exit(-1);
+        }
+        return squares;
+    }
+}
